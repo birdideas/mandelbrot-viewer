f2c876b523adc25f4cf7bdcfa52cde59f56b79e9
birdideas <birdideasblog@gmail.com>
2025-10-26 13:42:54 -0400

    BIG commit: pretty much an initial commit Program calculates the mandelbrot set, displays it on the screen, and saves a screenshot.
    A lot of functions were written for setting pixels on GTK, not really worth writing about them here.

--- Makefile
+++ Makefile
@@ -1,6 +1,2 @@
 build:
-	gcc src/main.c -o mand.exe -g -std=c99 -Wall -Wextra -pedantic -Wmissing-prototypes -Wstrict-prototypes -Werror
-build-o:
-	gcc src/main.c -o mand.exe -g -std=c99 -Wall -Wextra -pedantic -Wmissing-prototypes -Wstrict-prototypes -Werror -O3
-gtk:
-	gcc src/gtk.c `pkg-config --cflags --libs gtk+-3.0` -o gtk.exe -g -Wall -Wextra -pedantic -Wmissing-prototypes -Wstrict-prototypes
+	gcc src/main.c -o mand.exe `pkg-config --cflags --libs gtk4` -g -Wall -Wextra -pedantic -Wstrict-prototypes -Werror -O3 -lm
--- src/main.c
+++ src/main.c
@@ -14,63 +14,193 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-#include <stdio.h>
-#include <stdbool.h>
+/* #include <stdio.h> */
+#include <gtk/gtk.h>
+#include <time.h>
+#include <math.h>
 #include <complex.h>
 #include <tgmath.h>
 
-void print_c(double complex c);
-int escape(double complex z_0, int iterations);
+/* TODO:
+ * Allow for reloading the image without recompiling the program.
+ * Global variables bad
+ * Why are all the colors blue?
+ * Check if periodicity check is being called
+ */
 
-void print_c(double complex c)
+#define WIDTH  1000
+#define HEIGHT 1000
+
+static uint32_t pixels[WIDTH * HEIGHT];
+static GtkWidget *drawing_area = NULL;
+
+static void init_pixels(void)
 {
-     printf("%.5f%+.5fi\n", creal(c), cimag(c));
+    for (int i = 0; i < WIDTH * HEIGHT; i++)
+        pixels[i] = 0xFFFF0000;
+}
+
+static void set_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b)
+{
+    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
+        return;
+    }
+
+    pixels[y * WIDTH + x] = (0xFF << 24) | (r << 16) | (g << 8) | b;
+}
+
+static void on_draw(__attribute__((unused)) GtkDrawingArea *area, cairo_t *cr,
+                    __attribute__((unused)) int width,
+                    __attribute__((unused)) int height,
+                    __attribute__((unused)) gpointer data)
+{
+    cairo_surface_t *surface = cairo_image_surface_create_for_data(
+        (unsigned char *)pixels,
+        CAIRO_FORMAT_ARGB32,
+        WIDTH, HEIGHT,
+        WIDTH * 4
+    );
+    unsigned int t;
+    char str[23];
+
+    cairo_set_source_surface(cr, surface, 0, 0);
+    cairo_paint(cr);
+
+    t = time(NULL);
+    snprintf(str, 23, "images/%d.png", t);
+    cairo_surface_write_to_png(surface, str);
+
+    cairo_surface_destroy(surface);
+}
+
+/* I don't know how good this coloring function is yet. */
+int colors(int i)
+{
+    int ret = 0xFF000000;
+    int r = (((i % 2) + 1)<<5) - 1;
+    int g = (((i % 4) + 1)<<5) - 1;
+    int b = (((i % 8) + 1)<<5) - 1;
+
+    return (ret | r<<15 | g<<7 | b);
 }
 
 int escape(double complex z_0, int iterations)
 {
+    /* Counters: */
     int i = 0;
-    int x_squared, y_squared;
     double complex n = z_0;
+    double x, y;
 
+    /* Wikipedia.org: Plotting algorithms for the Mandelbrot set
+     * Under the heading "Periodicity checking" -- no author cited */
+    double x_old = 0, y_old = 0;
+    int period = 0;
+    int period_steps = 10;
+    double period_tolerance = 0.05;
+
+    /* Return the number of iterations to signal
+     * that the number zero did NOT escape */
     if (z_0 == 0) {
-        return false;
+        return iterations;
     }
 
     do {
-        x_squared = creal(n) * creal(n);
-        y_squared = cimag(n) * cimag(n);
+        x = creal(n);
+        y = cimag(n);
 
         n = (n*n + z_0);
-    } while ((x_squared + y_squared <= 4) && (++i < iterations));
 
-    return (i != iterations);
-}
-
-int main(void)
-{
-    int i;
-    int escape_success;
-    const int iter = 1000;
-
-    double complex tests[] = {1, 0};
-    double complex j;
-
-    for (i = 0; i < 2; ++i) {
-        j = tests[i];
-
-        escape_success = escape(j, iter);
-        print_c(j);
-
-        if (escape_success) {
-            puts("Escaped");
-        } else {
-            puts("Did not escape");
+        if (fabs(x_old - x) < period_tolerance &&
+            fabs(y_old - y) < period_tolerance)
+        {
+            return iterations;
         }
 
-        puts("");
-    }
+        if (period > period_steps) {
+            period = 0;
+            x_old = x;
+            y_old = y;
+        }
 
-    return 0;
+        ++period;
+    } while ((x*x + y*y <= 4) && (++i < iterations));
+
+    return i;
+}
+
+void mandelbrot_initial_loop(void)
+{
+    int width = WIDTH, height = HEIGHT;
+    double x, y;
+    int x_pix, y_pix;
+    double x_min = -2, y_min = -2;
+    double x_max = 2,  y_max = 2;
+    double dx = fabs(x_max - x_min) / width;
+    double dy = fabs(y_max - y_min) / height;
+
+    int escape_success;
+    int steps;
+    int c;
+    
+    double complex j;
+    const int iter = 1000;
+
+    for (y = y_min, y_pix = 0; y < y_max; y += dy, ++y_pix) {
+        for (x = x_min, x_pix = 0; x < x_max; x += dx, ++x_pix) {
+            j = (x + y*I);
+
+            steps = escape(j, iter);
+            c = colors(steps);
+            escape_success = (steps != iter);
+
+            if (escape_success) {
+                set_pixel(x_pix, y_pix, (c & 0x00FF0000)>>15, (c & 0x0000FF00)>>7, c & 0x000000FF);
+            } else {
+                set_pixel(x_pix, y_pix, 0, 0, 0);
+            }
+        }
+    }
+}
+
+static void activate
+(
+    GtkApplication *app,
+    __attribute__((unused)) gpointer user_data
+)
+{
+    init_pixels();
+
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Mandelbrot");
+    gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, HEIGHT);
+
+    drawing_area = gtk_drawing_area_new();
+    gtk_drawing_area_set_content_width(GTK_DRAWING_AREA(drawing_area), WIDTH);
+    gtk_drawing_area_set_content_height(GTK_DRAWING_AREA(drawing_area), HEIGHT);
+    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing_area), on_draw, NULL, NULL);
+
+    // Add click handler
+    GtkGesture *click = gtk_gesture_click_new();
+    /* g_signal_connect(click, "pressed", G_CALLBACK(on_click), NULL); */
+    gtk_widget_add_controller(drawing_area, GTK_EVENT_CONTROLLER(click));
+
+    gtk_window_set_child(GTK_WINDOW(window), drawing_area);
+
+    /* gtk_window_fullscreen(GTK_WINDOW(window)); */
+    mandelbrot_initial_loop();
+    gtk_widget_show(window);
+}
+
+int main(int argc, char *argv[])
+{
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.birdideas.mandelbrot", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    g_object_unref(app);
+    return status;
 }
 
